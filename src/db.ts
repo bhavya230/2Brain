95a82b2c45cb5044b3dbcedb41280d1d3c8bbfa0
unknown <raghavbhavyasingh@gmail.com>
2025-09-03 21:51:20 +0530

    database file

---
+++ src/db.ts
@@ -0,0 +1,7 @@
+import mongoose, { mongo, Mongoose } from "mongoose";
+const Schema=mongoose.Schema;
+
+mongoose.connect("mongodb+srv://bhavya:bha06%40SR23@cluster0.qlshlka.mongodb.net/2Bain");
+const ObjectId= mongoose.Types.ObjectId;
+
+//creating mongoose schemas
