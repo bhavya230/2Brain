9c0ea2595fd805331e567b96fb60b80ceb81d27d
unknown <raghavbhavyasingh@gmail.com>
2025-09-05 17:52:29 +0530

    User schema defined

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,19 @@
-import mongoose, { mongo, Mongoose } from "mongoose";
+import mongoose  from "mongoose";
+import 'dotenv/config'
 const Schema=mongoose.Schema;
+const MONGO_URL=process.env.MONGO_URL;
 
-mongoose.connect("");
+if(!MONGO_URL){
+    throw new Error("mongo url undefined")
+}
+
+mongoose.connect(MONGO_URL);
 const ObjectId= mongoose.Types.ObjectId;
 
 //creating mongoose schemas
+const Users=new Schema({
+    username:{type:String,unique:true},
+    password:String 
+})
+
+export const UsersModel=mongoose.model("users",Users);
